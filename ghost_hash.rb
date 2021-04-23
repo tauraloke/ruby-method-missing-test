f312ff31cafe101ae77a31cd8a492330ae053d93
tauraloke <tauraloke@gmail.com>
2021-04-23 12:52:12 +0300

    new version for solution

--- ghost_hash.rb
+++ ghost_hash.rb
@@ -2,7 +2,15 @@ require 'hashie'
 
 class GhostHash < Hashie::Mash
   class << self
-    attr_accessor :json_filename, :json_root
+    attr_accessor :json_filename, :json_root, :name
+  end
+
+  def initialize(hash)
+    result = hash.dup
+    result.each do |key, value|
+      result[key] = value
+    end
+    result
   end
 
   # redeclare Mash method for updating json file after property' changing
@@ -10,6 +18,24 @@ class GhostHash < Hashie::Mash
     super(name, value)
     serialize_root
     value
+  end
+
+  def convert_value(val, duping = false) #:nodoc:
+    case val
+    when self.class
+      val.dup
+    when Hash
+      duping ? val.dup : val
+    when ::Hash
+      val = val.dup if duping
+      new_class = Class.new(self.class)
+      print 'getting new class', new_class, '+++++++'
+      new_class.new(val)
+    when ::Array
+      Array.new(val.map { |e| convert_value(e) })
+    else
+      val
+    end
   end
 
   private
--- json_db.rb
+++ json_db.rb
@@ -1,36 +1,87 @@
 require 'json'
-require './ghost_hash'
+require 'active_support/inflector'
+#
+#require 'pry'
 
 class JsonDb
 
   ALLOWED_SUFFIXES = %w[=].freeze
-  
-  def initialize(json_filename)
-    @json_filename = json_filename
-    # create dynamic class because we send unique property of current instance to many custom hash instances
-    @ghost_hash_class = Class.new(GhostHash)
-    @ghost_hash_class.json_filename = @json_filename   # we need send it to any instances of hash class
-    @ghost_hash_class.json_root = self                 # and this one too
+  attr_reader :json_filename
+
+  class << self
+    attr_accessor :name
   end
 
-  attr_reader :json_filename
+  def initialize(data, classname = nil, root_json_db = nil)
+    @is_root = data.class.name == String.to_s
+    if @is_root
+      @json_filename = data
+      self.class.name = 'Root'
+      @root_json_db = self
+    else
+      @json_filename = root_json_db.json_filename
+      self.class.name = classname
+      @root_json_db = root_json_db
+      @data = redeclare_fields(data)
+    end
+  end
   
   def data
-    @data ||= @ghost_hash_class.new(JSON.parse(IO.read(json_filename)))
+    unless @data.present?
+      @data = redeclare_fields(JSON.parse(IO.read(@json_filename)))
+    end
+    @data
+  end
+
+  def [](field)
+    data[field.to_s]
+  end
+
+  def []=(field, value)
+    value = assign_property(field, value)
+    serialize
+    value
   end
   
   private
+
+  def redeclare_fields(hash)
+    hash.each do |key, value|
+      hash[key] = redeclare_object(value, key)
+    end
+    hash
+  end
+
+  def redeclare_object(data, key, new_name = nil)
+    case data.class.name
+    when Hash.to_s
+      self.class.new(data, new_name, self)
+    when Array.to_s
+      data.map do |row|
+        new_name = key_to_class_name(key)
+        row = redeclare_object(row, key, new_name)
+      end
+    else
+      data
+    end
+  end
+
+  def key_to_class_name(key)
+    ActiveSupport::Inflector.singularize(key).capitalize
+  end
   
   # Use this method to store updated properties on disk
   def serialize
-    IO.write(json_filename, data.to_json)
+    IO.write(json_filename, @root_json_db.data.to_json)
   end
 
   def method_missing(method_name, *args, &block)
     name, suffix = method_name_and_suffix(method_name.to_s)
     case suffix
     when '='.freeze
-      assign_property(name, args.first)
+      value = assign_property(name, args.first)
+      serialize
+      value
     else
       data[method_name.to_s]
     end
@@ -47,8 +98,6 @@ class JsonDb
 
   def assign_property(name, value)
     data[name] = value
-    serialize
-    value
   end
   
 end
